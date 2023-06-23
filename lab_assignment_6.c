e9996601466ccda72440999368ae3b855c799514
William <williamrmoulton@icloud.com>
2023-06-23 18:23:21 -0400

    Lab 6 using binary search

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int search(int numbers[], int low, int high, int value) 
 {
+	int mid;
+	if(low<=high){
+		mid = (low+high)/2;
+		if (value < numbers[mid])
+			return search(numbers,low,mid-1, value);
+		if(value > numbers[mid])
+			return search(numbers,mid+1,high,value);
+		else
+			return mid;
+		
+	}
 	return -1;
 }
 
@@ -57,4 +68,5 @@ int main(void)
 	}
 
 	fclose(inFile);
+	return 0;
 }
